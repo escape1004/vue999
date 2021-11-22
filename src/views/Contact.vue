d3d31f8b53e8b8896ac42f839e31798acb1c2d7d
Escape <65812015+escape1004@users.noreply.github.com>
2021-11-22 16:02:33 +0900

    2021.11.22v1
    :D

---
+++ src/views/Contact.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="light">
+    <Header />
+    <main id="main">
+      <section id="mainCont">
+        <h2 class="sr-only">연락처 컨텐츠 영역입니다.</h2>
+        <div class="main__cont tact">
+          <div class="l1">You are</div>
+          <div class="l2">already</div>
+          <div class="l3">doing well.</div>
+        </div>
+      </section>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+.main__cont.tact {
+  div:nth-child(2) {
+    margin-left: 9vw;
+  }
+  div:nth-child(3) {
+    margin-left: 18vw;
+  }
+}
+</style>
---
+++ src/views/Script.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="light">
+    <Header />
+    <main id="main">
+      <section id="scriptCont">
+        <div class="container">
+          <WrapTitle name1="Script" name2="Reference" />
+          <div class="script__cont"></div>
+        </div>
+      </section>
+      <ContInfo />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import WrapTitle from '@/components/WrapTitle.vue';
+import ContInfo from '@/components/ContInfo.vue';
+
+export default {
+  components: {
+    Header,
+    Footer,
+    WrapTitle,
+    ContInfo,
+  },
+};
+</script>
+
+<style lang="scss">
+#scriptCont {
+  background-color: #000;
+  color: #fff;
+}
+</style>
